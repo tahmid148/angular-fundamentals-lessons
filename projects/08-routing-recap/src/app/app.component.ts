9c255ceba44bdbdd146365000d7a1a297e831253
Tahmid Rahman <tr920@ic.ac.uk>
2024-06-08 20:01:42 +0100

    08-router-recap complete

--- projects/08-routing-recap/src/app/app.component.ts
+++ projects/08-routing-recap/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <h1>Welcome to {{ title }}!</h1>
+    <ol>
+      @for(productTitleEntry of productTitles; track productTitleEntry) {
+      <li>
+        <a [routerLink]="['details', $index]">{{ productTitleEntry }}</a>
+      </li>
+      }
+    </ol>
+    <router-outlet />
+  `,
 })
 export class AppComponent {
   title = '08-routing-recap';
