63bc53e26e71ead998595305d51560502299ff2a
Tahmid Rahman <tr920@ic.ac.uk>
2024-06-08 22:09:48 +0100

    11-dependency-injection completed

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './data';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  template: `
+    <h1>User Listing</h1>
+    @for(userEntry of userData; track userEntry) {
+    <app-user-info [user]="userEntry" />
+    }
+  `,
+  imports: [UserInfoComponent],
 })
 export class AppComponent {
-  constructor() {}
+  userService = inject(UserService);
+  userData: User[] = [];
+
+  constructor() {
+    this.userService.getUserData().then((data) => {
+      this.userData = data;
+    });
+  }
 }
