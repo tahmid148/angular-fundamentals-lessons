7ab76c0cf0bf259eb7b0d8fed0b240db05bdd166
Tahmid Rahman <tr920@ic.ac.uk>
2024-06-12 16:16:21 +0100

    12-singals completed

--- projects/12-angular-signals/src/app/app.component.ts
+++ projects/12-angular-signals/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from './todo';
 
@@ -6,9 +6,51 @@ import { Todo } from './todo';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
-  template: ` <h1>Building a TODO List</h1> `,
+  template: ` <h1>Building a TODO List</h1>
+    @for(todo of todos(); track todo){
+    <label
+      [ngStyle]="{
+        'text-decoration': todo.completed ? 'line-through' : 'none'
+      }"
+      >{{ todo.title }}fw</label
+    >
+    <input
+      type="checkbox"
+      [checked]="todo.completed"
+      (change)="updateTodo(todo)"
+    />
+    }`,
   styles: `label { display: block }`,
 })
 export class AppComponent {
-  updateTodo(todo: Todo) {}
+  todos = signal<Todo[]>([
+    {
+      id: 1,
+      title: 'Learn Angular',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Learn TypeScript',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Learn RxJS',
+      completed: false,
+    },
+  ]);
+
+  updateTodo(todo: Todo) {
+    this.todos.update((todoList) => {
+      todoList.map((todoEntry) => {
+        if (todoEntry === todo) {
+          todoEntry.completed = !todo.completed;
+        }
+      });
+      return todoList;
+    });
+  }
+
+  ngOnInit() {}
 }
